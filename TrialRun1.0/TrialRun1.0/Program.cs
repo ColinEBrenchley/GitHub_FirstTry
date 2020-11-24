2f320e127970dcaf411b3acf74ad07af5fc4b721
Colin E Brenchley <WeSevenB@msn.com>
2020-11-24 11:45:16 -0800

    Keep moving forward!

--- TrialRun1.0/TrialRun1.0/Program.cs
+++ TrialRun1.0/TrialRun1.0/Program.cs
@@ -7,6 +7,7 @@ namespace TrialRun1._0
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Life is Good!");
         }
     }
 }
